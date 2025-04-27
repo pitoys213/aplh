775ef23070b594057333e117e95c1ae0b61240a4
pitoys213 <keluargamm039@gmail.com>
2025-04-28 00:21:10 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('hehe').split('')
+    const titles = ('hehe😊😊').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
